04794c08051ad39360536f2a278da26588f0413d
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-10-11 20:35:32 +0530

    New file

---
+++ Day 7/Square.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter your number ";
+    cin >> num;
+
+    for (int row = 0; row < num; row++)
+    {
+        for (int col = 0; col < num; col++)
+        {
+            cout << "# ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
