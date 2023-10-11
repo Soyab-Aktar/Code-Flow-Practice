e44a5705353abdad8ca2fe8640119a62d3f213bb
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-10-11 20:46:10 +0530

    New file

---
+++ Day 7/HollowTriangle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter your number ";
+    cin >> num;
+
+    for (int row = 0; row < num; row++)
+    {
+        for (int col = 0; col < num; col++)
+        {
+            if (row == 0 || row == num - 1 || col == 0 || col == num - 1)
+            {
+                cout << "# ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
