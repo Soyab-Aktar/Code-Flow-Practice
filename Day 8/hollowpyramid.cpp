f4de12c142d92e60e46db621df873a9d5ebca81e
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-10-13 20:25:03 +0530

    New file

---
+++ Day 8/hollowpyramid.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter a number : ";
+    cin >> num;
+
+    for (int row = 0; row < num; row++)
+    {
+        for (int col = 0; col < num - row + 1; col++)
+        {
+            cout << " ";
+        }
+        for (int col = 0; col < row + 1; col++)
+        {
+            if (col == 0 || col == row + 1 - 1)
+            {
+                cout << "# ";
+            }
+            else
+            {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
