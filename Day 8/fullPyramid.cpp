b7cca606dafc9b6185fe61d9eb8b40b716231e74
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-10-13 20:18:24 +0530

    New file

---
+++ Day 8/fullPyramid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter a number : ";
+    cin >> num;
+
+    for (int row = 0; row < num; row++)
+    {
+        for (int col = 0; col < num - row; col++)
+        {
+            cout << " ";
+        }
+        for (int col = 0; col < row + 1; col++)
+        {
+            cout << "# ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
