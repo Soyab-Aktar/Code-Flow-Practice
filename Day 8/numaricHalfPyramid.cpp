9425c18c1d3dd1fbc75fce2e49c720d5621ca157
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-10-13 20:13:57 +0530

    New folder and new file

---
+++ Day 8/numaricHalfPyramid.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter a number : ";
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << j << " ";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
