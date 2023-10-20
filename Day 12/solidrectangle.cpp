f89526485c9c0c27be6791c23a6a5ac136803169
Soyab-Aktar <iamsoyabcode23@gmail.com>
2023-10-20 09:00:12 +0530

    New file

---
+++ Day 12/solidrectangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num;
+    cout << "Enter a number : ";
+    cin >> num;
+
+    for (int i = 0; i < num; i++)
+    {
+        for (int j = 0; j < num + 3; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
